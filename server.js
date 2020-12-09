c3c4a5281251205d2736a8509bb01573a73d0538
rhegde1uncc <rhegde1@uncc.edu>
2020-12-09 16:08:48 -0500

    commented gzip

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ let refreshToken;
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Content-type,Authorization');
-  res.setHeader('Content-Encoding', 'gzip');
+  //res.setHeader('Content-Encoding', 'gzip');
   next();
 });
 
